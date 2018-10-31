8ffe4e933ffa2d253ca0f0a33349129a3b0dbbdb
Kate Connolly <katherine.d.connolly@gmail.com>
2018-10-31 20:09:14 +0000

    First steps of Tiny App, oops I forgot to commit before now.

---
+++ express_server.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var app = express();
+var PORT = 8080; // default port 8080
+
+app.set("view engine", "ejs")
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.send("<html><head>Tiny App!!</head></html>");
+});
+
+// app.get("/hello", (req, res) => {
+//     res.send("<html><body>Hello<b>World</b></body></html>\n");
+// });
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id };
+  res.render("urls_show", templateVars);
+  });
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
