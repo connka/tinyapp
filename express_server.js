309f686c59ba9220224fd3e8362746aac009a146
Kate Connolly <katherine.d.connolly@gmail.com>
2018-11-05 05:05:10 +0000

    added edit function

--- express_server.js
+++ express_server.js
@@ -3,10 +3,12 @@ const bodyParser = require("body-parser");
 var app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 var PORT = 8080; // default port 8080
+const morgan = require('morgan')
 
 app.set("view engine", "ejs")
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(morgan('dev'))
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -25,26 +27,34 @@ var generateRandomString = function() {
   for(var i = 0; i < 6; i++) {
       string += possible.charAt(Math.floor(Math.random() * possible.length));
   }
-  return string;
+  return string;  
 }
 
 ///////////////GET ROUTES///////////////
+
+//new url
 app.get("/urls/new", (req, res) => {
+//  console.log('fired urlsnew')
   res.render("urls_new");
 });
 
+//update existing URL
 app.get("/urls/:id", (req, res) => {
+//  console.log('fired urls:id')
   let templateVars = { shortURL: req.params.id };
   res.render("urls_show", templateVars);
   });
 
+  //Displays Archive
 app.get("/urls", (req, res) => {
+//  console.log('urls')
     let templateVars = { urls: urlDatabase };
     res.render("urls_index", templateVars);
   });
 
 //redirects valid url
   app.get("/urls/:shortURL", (req, res) => {
+//    console.log('/urls/:shortURL')
     if(urlDatabas[req.params.shortURL]){
       res.statusCode = 308;
       res.redirect(`${urlDatabase[req.params.shortURL].url}`);
@@ -55,20 +65,43 @@ app.get("/urls", (req, res) => {
   
   // Generates random URL
   app.post("/urls", (req, res) => {
+    //console.log('/urlspost')
     console.log(req.body);
     const newID = generateRandomString(); 
     urlDatabase[newID] = req.body.longURL;
     console.log(urlDatabase);
-    res.redirect("/urls/");
+    res.redirect('/urls/');
 }); 
 
 // Delete a url
 app.post("/urls/:shortURLID/delete", (req, res) => {
+  //console.log('urls/:shortURLID')
     delete urlDatabase[req.params.shortURLID];
     res.redirect(`/urls`);
+  });
+
+  app.get("/urls/:id/", (req, res) => {
+    //console.log('urls/id/ GET')
+    let templateVars = {
+      shortURL: req.params.id,
+      longURL: urlDatabase[req.params.id],
+    };
+      res.render(`/urls/:id/`);
+  });
+  
+  //Submit an edit url
+  app.get("/urls/:id", (req, res) => {
+    //  console.log('fired urls:id')
+      let templateVars = { shortURL: req.params.id };
+      res.render("urls_show", templateVars);
+      });
+
+  app.post("/urls/:id/", (req, res) => {
+    console.log("the post" + req.body.longURL)
+    urlDatabase[req.params.id] = req.body.longURL;
+    res.redirect("/urls/");
   });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
